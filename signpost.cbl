2d49900ff97a9e089679201c64ae2302d5dd758e
Jo-Jo-Bean <milocarteret@gmail.com>
2026-05-08 08:55:53 -0500

    Update signpost.cbl
    Added a dictionary for arrow directions
    Modified the display output of the game

--- signpost.cbl
+++ signpost.cbl
@@ -1,33 +1,46 @@
       *    Written By:  Jo-Jo-Bean   
       *    Created On:  2026-05-07
-      *    Last Edited: 2026-05-07
-      *    Purpose:
-      *    My attempt to rewrite Simon Tatham's Signpost puzzle from
-      *    scratch in COBOL! I don't understand enough about C to read
-      *    the code on his GitHub, but I think I got enough from the
-      *    comments in the code there to work the rest out myself.
-      *    Here goes nothing!
+      *    Last Edited: 2026-05-08
+      *    Purpose: My attempt to rewrite Simon Tatham's Signpost puzzle
+      *        from scratch in COBOL!
+      *
        IDENTIFICATION DIVISION.
        PROGRAM-ID. SIGNPOST.
       *
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 FIELD-SIZE PIC 9 VALUE IS 5.
-       01 FIELD.
-           05 FIELD-X OCCURS 9 TIMES.
-               10 FIELD-Y OCCURS 9 TIMES.
-                   15 ARROW-TRUE PIC 9.
-                   15 ARROW-SET PIC 9.
-                   15 SEQ PIC 99.
-                   15 PATH PIC X.
+           01 FIELD-SIZE PIC 9 VALUE IS 5.
+           01 FIELD.
+               05 FIELD-X OCCURS 9 TIMES.
+                   10 FIELD-Y OCCURS 9 TIMES.
+                       15 ARROW-TRUE PIC 9.
+                       15 ARROW-SET PIC 9 VALUE IS 9.
+                       15 SEQ PIC 99.
+                       15 PATH PIC X.
+    
+           01 ARROW-TABLE-DATA.
+               05 FILLER PIC X(2) VALUE IS 'N '.
+               05 FILLER PIC X(2) VALUE IS 'NE'.
+               05 FILLER PIC X(2) VALUE IS 'E '.
+               05 FILLER PIC X(2) VALUE IS 'SE'.
+               05 FILLER PIC X(2) VALUE IS 'S '.
+               05 FILLER PIC X(2) VALUE IS 'SW'.
+               05 FILLER PIC X(2) VALUE IS 'W '.
+               05 FILLER PIC X(2) VALUE IS 'NW'.
+               05 FILLER PIC X(2) VALUE IS '--'.
 
-       01 X PIC 9.
-       01 Y PIC 9.
-
-       
-       01 REPLAY PIC X.
-       01 GAME-DONE PIC 9.
-
+           01 ARROW-TABLE REDEFINES ARROW-TABLE-DATA.
+               05 ARROW-MAP OCCURS 9 TIMES PIC X(2).
+           01 ARROW-MAP-SIZE PIC 9 VALUE IS 9.
+    
+           01 X PIC 99.
+           01 Y PIC 99.
+           01 COL-X PIC 9.
+           01 COL-Y PIC 9.                                                 
+    
+           
+           01 REPLAY PIC X.
+           01 GAME-DONE PIC 9.
       *
        PROCEDURE DIVISION.
        MAIN-PROGRAM.
@@ -41,8 +54,7 @@
            PERFORM UNTIL Y > FIELD-SIZE
                MOVE 1 TO X
                    PERFORM UNTIL X > FIELD-SIZE
-                       MOVE Y TO ARROW-SET(X,Y) ARROW-TRUE(X,Y)
-                                 SEQ(X,Y) PATH(X,Y) 
+                       COMPUTE SEQ(X, Y) = X + ((Y - 1) * FIELD-SIZE)
                        ADD 1 TO X
                    END-PERFORM
                ADD 1 TO Y
@@ -55,15 +67,29 @@
        PRINT-GAME.
            MOVE 1 TO Y
            PERFORM UNTIL Y > FIELD-SIZE
+               SUBTRACT Y -1 FROM FIELD-SIZE GIVING COL-Y
+               DISPLAY COL-Y " " WITH NO ADVANCING
                MOVE 1 TO X
-                   PERFORM UNTIL X > FIELD-SIZE
-                       DISPLAY ARROW-SET(X,Y) " " ARROW-TRUE(X,Y) " "
-                               SEQ(X,Y) " " PATH(X,Y) " " 
-                       WITH NO ADVANCING     
-                       ADD 1 TO X
-                   END-PERFORM
+               PERFORM UNTIL X > FIELD-SIZE
+                   DISPLAY SEQ(X,Y) "  " WITH NO ADVANCING     
+                   ADD 1 TO X
+               END-PERFORM
+               DISPLAY " "
+               DISPLAY "  " WITH NO ADVANCING
+               MOVE 1 TO X
+               PERFORM UNTIL X > FIELD-SIZE
+                   DISPLAY ARROW-MAP(ARROW-SET(X,Y)) "  "
+                       WITH NO ADVANCING    
+                   ADD 1 TO X
+               END-PERFORM
                DISPLAY " "
                ADD 1 TO Y
+           END-PERFORM.
+           DISPLAY " ".
+           MOVE 1 TO X.
+           PERFORM UNTIL X > ARROW-MAP-SIZE
+               DISPLAY ARROW-MAP(X) " " WITH NO ADVANCING
+               ADD 1 TO X
            END-PERFORM.
 
        NEW-GAME.
@@ -83,8 +109,7 @@
            PERFORM UNTIL Y > FIELD-SIZE
                MOVE 1 TO X
                    PERFORM UNTIL X > FIELD-SIZE
-                       MOVE 0 TO ARROW-SET(X,Y) ARROW-TRUE(X,Y)
-                                 SEQ(X,Y) PATH(X,Y)
+                       MOVE 9 TO ARROW-SET(X,Y)
                        ADD 1 TO X
                    END-PERFORM
                ADD 1 TO Y
